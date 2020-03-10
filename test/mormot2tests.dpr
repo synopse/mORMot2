c5a10428caa41a4adca0e414291b6edf3ce69c36
Arnaud Bouchez <fakeaddress@synopse.info>
2020-03-10 13:37:35 +0100

    updated core.base

--- test/mormot2tests.dpr
+++ test/mormot2tests.dpr
@@ -1,19 +1,28 @@
-/// framework unit, performance and regression tests for continuous integration
-// - this unit is a part of the freeware Synopse mORMot framework 2,
+/// framework unitary, performance and regression tests for continuous integration
+// - this program is a part of the freeware Synopse mORMot framework 2,
 // licensed under a MPL/GPL/LGPL three license - see LICENSE.md
 program mormot2tests;
 
-{$I mormot.defines.inc}
+{$I ..\src\mormot.defines.inc}
 
 {$ifdef MSWINDOWS}
   {$apptype console}
   {$R ..\src\mormot.win.default.manifest.res}
 {$endif MSWINDOWS}
 
-uses 
-  mormot.core.base;
+uses
+  {$I ..\src\mormot.uses.inc}
+  mormot.core.base in '..\src\core\mormot.core.base.pas',
+  mormot.core.os in '..\src\core\mormot.core.os.pas',
+  mormot.core.text in '..\src\core\mormot.core.text.pas',
+  mormot.core.data in '..\src\core\mormot.core.data.pas',
+  mormot.core.datetime in '..\src\core\mormot.core.datetime.pas',
+  mormot.core.json in '..\src\core\mormot.core.json.pas',
+  mormot.core.variants in '..\src\core\mormot.core.variants.pas';
+
 
 begin
   writeln('mORMot ' + SYNOPSE_FRAMEWORK_FULLVERSION + ' compiles!');
   readln;
 end.
+
