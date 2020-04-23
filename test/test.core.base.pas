37b521d036260cb6520e418c585bfa128ec03ca5
Arnaud Bouchez <fakeaddress@synopse.info>
2020-04-23 16:44:44 +0200

    added missing unit test.core.base.pas

---
+++ test/test.core.base.pas
@@ -0,0 +1,24 @@
+/// regression tests for mormot.core.base features
+// - this unit is a part of the freeware Synopse mORMot framework 2,
+// licensed under a MPL/GPL/LGPL three license - see LICENSE.md
+unit test.core.base;
+
+interface
+
+{$I ..\src\mormot.defines.inc}
+
+uses
+  mormot.core.base,
+  mormot.core.test;
+
+type
+  /// regression tests for mormot.core.base features
+  TTestCoreBase = class(TSynTestCase)
+  published
+  end;
+
+implementation
+
+
+
+end.
