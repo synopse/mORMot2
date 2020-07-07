6a56c41d661adfc7c773db5c999571558bddf1bd
Arnaud Bouchez <fakeaddress@synopse.info>
2020-07-07 18:03:24 +0200

    introducing THttpSocket class

---
+++ src/net/mormot.net.server.pas
@@ -0,0 +1,41 @@
+/// HTTP/HTTPS Server Classes
+// - this unit is a part of the freeware Synopse mORMot framework 2,
+// licensed under a MPL/GPL/LGPL three license - see LICENSE.md
+unit mormot.net.server;
+
+{
+  *****************************************************************************
+
+   HTTP Server Classes
+   - 
+
+
+  *****************************************************************************
+
+}
+
+interface
+
+{$I ..\mormot.defines.inc}
+
+uses
+  sysutils,
+  classes,
+  mormot.core.base,
+  mormot.core.os,
+  mormot.net.sock,
+  mormot.net.client;
+
+
+{ ********************  }
+
+
+implementation
+
+{ ********************  }
+
+initialization
+
+finalization
+end.
+
