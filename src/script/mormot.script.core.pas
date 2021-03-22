16fe94bc780f291c08e433e0e7f8969cc8b2703c
Arnaud Bouchez <fakeaddress@synopse.info>
2021-03-22 20:12:27 +0100

    introducing new src/script folder

---
+++ src/script/mormot.script.core.pas
@@ -0,0 +1,29 @@
+/// Scripting Integration to our Framework
+// - this unit is a part of the Open Source Synopse mORMot framework 2,
+// licensed under a MPL/GPL/LGPL three license - see LICENSE.md
+unit mormot.script.core;
+
+{
+  *****************************************************************************
+
+   Abstract Types for Generic Scripting Integration
+    - Generic Execution Engine Parent class
+    - 
+
+  *****************************************************************************
+}
+
+interface
+
+{$I ..\mormot.defines.inc}
+
+uses
+  sysutils,
+  classes,
+  variants,
+  mormot.core.base;
+  
+implementation
+
+end.
+
