d538f0013f0384f5153944c137132992673fb6ed
Arnaud Bouchez <ab@synopse.info>
2021-06-23 16:41:48 +0200

    TGenerics renamed as Collections static class - including big code simplication; even if Delphi has still sometimes internal errors :(

--- test/compilXE7.bat
+++ test/compilXE7.bat
@@ -1,13 +1,13 @@
 @echo off
 cls
 
-set DCC="c:\progs\Embarcadero\XE7\bin\fastdcc32.exe"
+set DCC="c:\progs\DelphiXE7\bin\fastdcc32.exe"
 if not exist %DCC% set DCC="c:\progs\DelphiXE7\bin\dcc32.exe"
 set DelphiVersion=Delphi XE7
 set target=Win32
 call compilpil.bat
 
-set DCC="c:\progs\Embarcadero\XE7\bin\fastdcc64.exe"
+set DCC="c:\progs\DelphiXE7\bin\fastdcc64.exe"
 if not exist %DCC% set DCC="c:\progs\DelphiXE7\bin\dcc64.exe"
 set DelphiVersion=Delphi XE7
 set target=Win64
