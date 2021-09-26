73eefaa45de3b3eb1e22aad0d6886ba16c77aefc
Home8 <tom@halkyon.com>
2021-09-27 01:20:54 +0200

    Sample 15-External DB performance conversion with added ibx test

---
+++ ex/extdb-bench/PerfTestConsole.dpr
@@ -0,0 +1,18 @@
+program PerfTestConsole;
+
+{$I mormot.defines.inc}
+
+{$ifdef OSWINDOWS}
+  {$apptype console}
+  {../../src/$R mormot.win.default.manifest.res}
+{$endif OSWINDOWS}
+
+uses
+  {$I mormot.uses.inc}
+  mormot.core.log,
+  PerfTestCases in '.\PerfTestCases.pas';
+
+begin
+  TTestDatabaseBenchmark.RunAsConsole(
+    'mORMot Framework Database Benchmark'{, LOG_VERBOSE});
+end.
