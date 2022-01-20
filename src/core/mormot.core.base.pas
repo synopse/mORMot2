7f6de8158b4e990f5ab818cd40d506b0dee02b8d
Arnaud Bouchez <ab@synopse.info>
2022-01-20 18:44:06 +0100

    new overloaded PosChar() with length parameter

--- src/core/mormot.core.base.pas
+++ src/core/mormot.core.base.pas
@@ -829,16 +829,16 @@ const
   /// fast lookup table for converting any decimal number from
   // 0 to 99 into their ASCII equivalence
   TwoDigitLookup: packed array[0..99] of array[1..2] of AnsiChar =
-    ('00','01','02','03','04','05','06','07','08','09',
-     '10','11','12','13','14','15','16','17','18','19',
-     '20','21','22','23','24','25','26','27','28','29',
-     '30','31','32','33','34','35','36','37','38','39',
-     '40','41','42','43','44','45','46','47','48','49',
-     '50','51','52','53','54','55','56','57','58','59',
-     '60','61','62','63','64','65','66','67','68','69',
-     '70','71','72','73','74','75','76','77','78','79',
-     '80','81','82','83','84','85','86','87','88','89',
-     '90','91','92','93','94','95','96','97','98','99');
+    ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+     '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+     '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+     '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+     '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
+     '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
+     '60', '61', '62', '63', '64', '65', '66', '67', '68', '69',
+     '70', '71', '72', '73', '74', '75', '76', '77', '78', '79',
+     '80', '81', '82', '83', '84', '85', '86', '87', '88', '89',
+     '90', '91', '92', '93', '94', '95', '96', '97', '98', '99');
 
 var
   /// fast lookup table for converting any decimal number from
@@ -2560,9 +2560,14 @@ function PosExChar(Chr: AnsiChar; const Str: RawUtf8): PtrInt;
   {$ifdef HASINLINE}inline;{$endif}
 
 /// fast retrieve the position of a given character in a #0 ended buffer
-// - will use fast SSE2 asm on x86_64
-function PosChar(Str: PUtf8Char; Chr: AnsiChar): PUtf8Char;
+// - will use fast SSE2 asm on i386 and x86_64
+function PosChar(Str: PUtf8Char; Chr: AnsiChar): PUtf8Char; overload;
   {$ifndef CPUX64}{$ifdef FPC}inline;{$endif}{$endif}
+
+/// fast retrieve the position of a given character in a #0 ended buffer
+// - will use fast SSE2 asm on i386 and x86_64
+function PosChar(Str: PUtf8Char; StrLen: PtrInt; Chr: AnsiChar): PUtf8Char; overload;
+  {$ifdef HASINLINE}inline;{$endif}
 
 {$ifndef PUREMORMOT2}
 /// fast dedicated RawUtf8 version of Trim()
@@ -7486,6 +7491,20 @@ begin
     result := ByteScanIndex(pointer(Str), PStrLen(PtrUInt(Str) - _STRLEN)^, byte(Chr)) + 1
   else
     result := 0;
+end;
+
+function PosChar(Str: PUtf8Char; StrLen: PtrInt; Chr: AnsiChar): PUtf8Char;
+begin
+  if StrLen <> 0 then
+  begin
+    StrLen := ByteScanIndex(pointer(Str), StrLen, byte(Chr));
+    if StrLen >= 0 then
+      result := Str + StrLen
+    else
+      result := nil;
+  end
+  else
+    result := nil;
 end;
 
 {$ifdef UNICODE}
