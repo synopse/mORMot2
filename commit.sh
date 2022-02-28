6fb32b6d891364b6425aaa7a6895604be4ff8ce9
Arnaud Bouchez <ab@synopse.info>
2022-02-28 10:58:35 +0100

    automatically increase build number on each commit

--- commit.sh
+++ commit.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+VERS=`cat src/mormot.commit.inc|sed 's/.*2.0.//; s/''*//' `
+#echo VERS=$VERS
+V2=${VERS::-2}
+#echo V2=$V2
+V2=$((V2+1))
+V2=\'2.0.$V2\'f
+echo $V2|tr "f" "\r\n">src/mormot.commit.inc
+
 git add --all
 git commit
 git push
 
-echo https://github.com/synopse/mORMot2/commit/`git rev-parse --short HEAD`
+echo $V2 as https://github.com/synopse/mORMot2/commit/`git rev-parse --short HEAD`
