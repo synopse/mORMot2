399bf76aa57a83d23b91c642aac9d14c55cd4b22
Arnaud Bouchez <ab@synopse.info>
2022-03-03 17:27:53 +0100

    fixed build number sequence at commit

--- commit.sh
+++ commit.sh
@@ -5,8 +5,7 @@ VERS=`cat src/mormot.commit.inc|sed 's/.*2.0.//; s/''*//' `
 V2=${VERS::-2}
 #echo V2=$V2
 V2=$((V2+1))
-V3=\'2.0.$V2\'f
-echo $V3|tr "f" "\r\n">src/mormot.commit.inc
+echo -e "'2.0.$V2'\r">src/mormot.commit.inc
 
 git add --all
 git commit
