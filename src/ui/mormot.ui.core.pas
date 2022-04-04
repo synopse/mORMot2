e97d7032678721a62e2cc7b5dbcbc84041f389a5
Arnaud Bouchez <ab@synopse.info>
2022-04-04 15:09:24 +0200

    completed LCL/VCL cross-compatibility definitions

--- src/ui/mormot.ui.core.pas
+++ src/ui/mormot.ui.core.pas
@@ -132,21 +132,34 @@ const
   EMR_POLYBEZIER = 2;
   EMR_POLYGON = 3;
   EMR_POLYLINE = 4;
+  EMR_POLYBEZIERTO = 5;
+  EMR_POLYLINETO = 6;
+  EMR_POLYPOLYLINE = 7;
+  EMR_POLYPOLYGON = 8;
   EMR_SETWINDOWEXTEX = 9;
   EMR_SETWINDOWORGEX = 10;
   EMR_SETVIEWPORTEXTEX = 11;
   EMR_SETVIEWPORTORGEX = 12;
+  EMR_SETBRUSHORGEX = 13;
+  EMR_EOF = 14;
+  EMR_SETPIXELV = 15;
+  EMR_SETMAPMODE = 17;
   EMR_SETBKMODE = 18;
+  EMR_SETPOLYFILLMODE = 19;
+  EMR_SETROP2 = 20;
+  EMR_SETSTRETCHBLTMODE = 21;
   EMR_SETTEXTALIGN = 22;
   EMR_SETTEXTCOLOR = 24;
   EMR_SETBKCOLOR = 25;
   EMR_OFFSETCLIPRGN = 26;
   EMR_MOVETOEX = 27;
+  EMR_SETMETARGN = 28;
   EMR_EXCLUDECLIPRECT = 29;
   EMR_INTERSECTCLIPRECT = 30;
   EMR_SAVEDC = 33;
   EMR_RESTOREDC = 34;
   EMR_SETWORLDTRANSFORM = 35;
+  EMR_MODIFYWORLDTRANSFORM = 36;
   EMR_SELECTOBJECT = 37;
   EMR_CREATEPEN = 38;
   EMR_CREATEBRUSHINDIRECT = 39;
@@ -154,17 +167,86 @@ const
   EMR_ELLIPSE = 42;
   EMR_RECTANGLE = 43;
   EMR_ROUNDRECT = 44;
+  EMR_ARC = 45;
+  EMR_CHORD = 46;
+  EMR_PIE = 47;
+  EMR_SELECTPALETTE = 48;
+  EMR_CREATEPALETTE = 49;
+  EMR_SETPALETTEENTRIES = 50;
+  EMR_RESIZEPALETTE = 51;
+  EMR_REALIZEPALETTE = 52;
+  EMR_EXTFLOODFILL = 53;
   EMR_LINETO = 54;
+  EMR_ARCTO = 55;
+  EMR_POLYDRAW = 56;
+  EMR_SETARCDIRECTION = 57;
+  EMR_SETMITERLIMIT = 58;
+  EMR_BEGINPATH = 59;
+  EMR_ENDPATH = 60;
+  EMR_CLOSEFIGURE = 61;
+  EMR_FILLPATH = 62;
+  EMR_STROKEANDFILLPATH = 63;
+  EMR_STROKEPATH = 64;
+  EMR_FLATTENPATH = 65;
+  EMR_WIDENPATH = 66;
   EMR_SELECTCLIPPATH = 67;
+  EMR_ABORTPATH = 68;
+  EMR_GDICOMMENT = 70;
+  EMR_FILLRGN = 71;
   EMR_EXTSELECTCLIPRGN = 75;
   EMR_BITBLT = 76;
   EMR_STRETCHBLT = 77;
   EMR_STRETCHDIBITS = 81;
   EMR_EXTCREATEFONTINDIRECTW = 82;
+  EMR_EXTTEXTOUTA = 83;
   EMR_EXTTEXTOUTW = 84;
   EMR_POLYBEZIER16 = 85;
   EMR_POLYGON16 = 86;
   EMR_POLYLINE16 = 87;
+  EMR_POLYBEZIERTO16 = 88;
+  EMR_POLYLINETO16 = 89;
+  EMR_POLYPOLYLINE16 = 90;
+  EMR_POLYPOLYGON16 = 91;
+  EMR_POLYDRAW16 = 92;
+  EMR_EXTCREATEPEN = 95;
+  EMR_SETICMMODE = 98;
+  EMR_SMALLTEXTOUT = 108;
+  EMR_ALPHADIBBLEND = 115;
+  EMR_TRANSPARENTBLT = 116;
+  EMR_GRADIENTFILL = 118;
+
+type
+  PEMRTransparentBLT = ^TEMRTransparentBLT;
+  TEMRTransparentBLT = packed record
+    emr: TEMR;
+    rclBounds: TRect;
+    xDest: Longint;
+    yDest: Longint;
+    cxDest: Longint;
+    cyDest: Longint;
+    dwRop: DWORD;
+    xSrc: Longint;
+    ySrc: Longint;
+    xformSrc: XFORM;         { Source DC transform}
+    crBkColorSrc: COLORREF;  { Source DC BkColor in RGB}
+    iUsageSrc: DWORD;        { Source bitmap info color table usage}
+    offBmiSrc: DWORD;        { Offset to the source TBitmapInfo structure}
+    cbBmiSrc: DWORD;         { Size of the source TBitmapInfo structure}
+    offBitsSrc: DWORD;       { Offset to the source bitmap bits}
+    cbBitsSrc: DWORD;        { Size of the source bitmap bits}
+    cxSrc: Longint;
+    cySrc: Longint;
+  end;
+
+  PEMGradientFill = ^TEMGradientFill;
+  TEMGradientFill = record
+    emr: TEMR;
+    rclBounds: TRect;   { Inclusive-inclusive bounds in device units}
+    nVer: DWORD;
+    nTri: DWORD;
+    ulMode: ULONG;
+    Ver: array[0..0] of TTriVertex;
+  end;
 
 {$endif OSWINDOWS}
 
