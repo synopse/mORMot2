40dd0526cfd4f588277fc7a1e37b008dde8a1079
Arnaud Bouchez <ab@synopse.info>
2022-04-19 15:42:23 +0200

    fixed commit script - bash is just buggy: use python instead

--- commit.sh
+++ commit.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-VERS=`cat src/mormot.commit.inc|sed 's/.*2.0.//; s/''*//' `
+function next_version {
+python - <<END
+with open("src/mormot.commit.inc") as file:
+  print (int(file.readline().replace("'", "")[4:])+1)
+END
+}
+
+VERS=$(next_version)
 #echo VERS=$VERS
-V2=${VERS::-2}
-#echo V2=$V2
-V2=$((V2+1))
-echo -e "'2.0.$V2'\r">src/mormot.commit.inc
+
+echo -e "'2.0.$VERS'\r">src/mormot.commit.inc
 cp src/mormot.commit.inc ~/dev/lib2/src/mormot.commit.inc
 
 git add --all
 git commit
 git push
 
-echo committed 2.0.$V2 as https://github.com/synopse/mORMot2/commit/`git rev-parse --short HEAD`
+echo committed 2.0.$VERS as https://github.com/synopse/mORMot2/commit/`git rev-parse --short HEAD`
