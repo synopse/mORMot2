b5106a2cdcd31b4849b763396cf09603696dda5d
Arnaud Bouchez <ab@synopse.info>
2023-01-04 15:23:39 +0100

    changed UrlDecodeParam() and associated HTTP server process

--- src/net/mormot.net.http.pas
+++ src/net/mormot.net.http.pas
@@ -144,18 +144,19 @@ function IsUrlFavIcon(P: PUtf8Char): boolean;
   {$ifdef HASINLINE} inline; {$endif}
 
 /// decode a given parameter from an Url, in any position, into UTF-8 text
+// - P^ should be either nil or point to P^ = '?'
 // - UpperName should follow the UrlDecodeValue() format, e.g. 'NAME='
-function UrlDecodeParam(const Url, UpperName: RawUtf8;
+function UrlDecodeParam(P: PUtf8Char; const UpperName: RawUtf8;
   out Value: RawUtf8): boolean; overload;
 
 /// decode a given parameter from an Url, in any position, into a 32-bit cardinal
 // - UpperName should follow the UrlDecodeCardinal() format, e.g. 'COUNT='
-function UrlDecodeParam(const Url, UpperName: RawUtf8;
+function UrlDecodeParam(P: PUtf8Char; const UpperName: RawUtf8;
   out Value: cardinal): boolean; overload;
 
 /// decode a given parameter from an Url, in any position, into a 64-bit Int64
 // - UpperName should follow the UrlDecodeInt64() format, e.g. 'ID='
-function UrlDecodeParam(const Url, UpperName: RawUtf8;
+function UrlDecodeParam(P: PUtf8Char; const UpperName: RawUtf8;
   out Value: Int64): boolean; overload;
 
 const
@@ -553,6 +554,7 @@ type
     fRespStatus: integer;
     fConnectionThread: TSynThread;
     fConnectionOpaque: PHttpServerConnectionOpaque;
+    fUrlParamPos: PUtf8Char; // may be set by TUriTreeNode.LookupParam
     fRouteName: pointer; // = pointer(TUriTreeNodeData.Names)
     fRouteValuePosLen: TIntegerDynArray; // [pos1,len1,...] pairs in fUri
     function GetRouteValuePosLen(const Name: RawUtf8): PIntegerArray;
@@ -608,15 +610,12 @@ type
     /// retrieve and decode an URI-encoded parameter as UTF-8 text
     // - UpperName should follow the UrlDecodeValue() format, e.g. 'NAME='
     function UrlParam(const UpperName: RawUtf8; out Value: RawUtf8): boolean; overload;
-      {$ifdef HASINLINE} inline; {$endif}
     /// retrieve and decode an URI-encoded parameter as 32-bit unsigned cardinal
     // - UpperName should follow the UrlDecodeCardinal() format, e.g. 'COUNT='
     function UrlParam(const UpperName: RawUtf8; out Value: cardinal): boolean; overload;
-      {$ifdef HASINLINE} inline; {$endif}
     /// retrieve and decode an URI-encoded parameter as 64-bit signed Int64
     // - UpperName should follow the UrlDecodeInt64() format, e.g. 'ID='
     function UrlParam(const UpperName: RawUtf8; out Value: Int64): boolean; overload;
-      {$ifdef HASINLINE} inline; {$endif}
     /// set the OutContent and OutContentType fields with the supplied JSON
     procedure SetOutJson(const Json: RawUtf8); overload;
       {$ifdef HASINLINE} inline; {$endif}
@@ -1108,53 +1107,47 @@ begin
   end;
 end;
 
-function UrlDecodeParam(const Url, UpperName: RawUtf8; out Value: RawUtf8): boolean;
-var
-  p: PUtf8Char;
+function UrlDecodeParam(P: PUtf8Char; const UpperName: RawUtf8;
+  out Value: RawUtf8): boolean;
 begin
-  p := PosChar(pointer(Url), '?');
-  if p <> nil then
+  if P <> nil then
   begin
     result := true;
-    inc(p);
+    inc(P);
     repeat
-      if UrlDecodeValue(p, UpperName, Value, @p) then
+      if UrlDecodeValue(P, UpperName, Value, @P) then
         exit;
-    until p = nil;
+    until P = nil;
   end;
   result := false;
 end;
 
-function UrlDecodeParam(const Url, UpperName: RawUtf8; out Value: cardinal): boolean;
-var
-  p: PUtf8Char;
+function UrlDecodeParam(P: PUtf8Char; const UpperName: RawUtf8;
+  out Value: cardinal): boolean;
 begin
-  p := PosChar(pointer(Url), '?');
-  if p <> nil then
+  if P <> nil then
   begin
     result := true;
-    inc(p);
+    inc(P);
     repeat
-      if UrlDecodeCardinal(p, UpperName, Value, @p) then
+      if UrlDecodeCardinal(P, UpperName, Value, @P) then
         exit;
-    until p = nil;
+    until P = nil;
   end;
   result := false;
 end;
 
-function UrlDecodeParam(const Url, UpperName: RawUtf8; out Value: Int64): boolean;
-var
-  p: PUtf8Char;
+function UrlDecodeParam(P: PUtf8Char; const UpperName: RawUtf8;
+  out Value: Int64): boolean;
 begin
-  p := PosChar(pointer(Url), '?');
-  if p <> nil then
+  if P <> nil then
   begin
     result := true;
-    inc(p);
+    inc(P);
     repeat
-      if UrlDecodeInt64(p, UpperName, Value, @p) then
+      if UrlDecodeInt64(P, UpperName, Value, @P) then
         exit;
-    until p = nil;
+    until P = nil;
   end;
   result := false;
 end;
@@ -2226,19 +2219,25 @@ end;
 function THttpServerRequestAbstract.UrlParam(const UpperName: RawUtf8;
   out Value: RawUtf8): boolean;
 begin
-  result := UrlDecodeParam(Url, UpperName, Value);
+  if fUrlParamPos = nil then // may have been set by TUriTreeNode.LookupParam
+    fUrlParamPos := PosChar(pointer(Url), '?');
+  result := UrlDecodeParam(fUrlParamPos, UpperName, Value);
 end;
 
 function THttpServerRequestAbstract.UrlParam(const UpperName: RawUtf8;
   out Value: cardinal): boolean;
 begin
-  result := UrlDecodeParam(Url, UpperName, Value);
+  if fUrlParamPos = nil then
+    fUrlParamPos := PosChar(pointer(Url), '?');
+  result := UrlDecodeParam(fUrlParamPos, UpperName, Value);
 end;
 
 function THttpServerRequestAbstract.UrlParam(const UpperName: RawUtf8;
   out Value: Int64): boolean;
 begin
-  result := UrlDecodeParam(Url, UpperName, Value);
+  if fUrlParamPos = nil then
+    fUrlParamPos := PosChar(pointer(Url), '?');
+  result := UrlDecodeParam(fUrlParamPos, UpperName, Value);
 end;
 
 procedure THttpServerRequestAbstract.SetOutJson(const Json: RawUtf8);
