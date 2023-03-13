synopse/mORMot2

commit 2c2a774bdac4a724d9a740ac7d83be3b247a762f

hardened TRawByteStringStream.GetAsText on FPC

src/core/mormot.core.base.pas
@@ -3742,6 +3742,7 @@ type
     procedure SetSize(NewSize: Longint); override;
   public
     /// initialize the storage, optionally with some RawByteString content
+    // - to be used for Read() from this memory buffer
     constructor Create(const aString: RawByteString); overload;
     /// read some bytes from the internal storage
     // - returns the number of bytes filled into Buffer (<=Count)
@@ -11545,7 +11546,12 @@ begin
   L := length(fDataString);
   if (StartPos = 0) and
      (Len = L) then
-    FastAssignUtf8(Text, fDataString) // FPC expects this
+  begin
+    {$ifdef HASCODEPAGE}
+    SetCodePage(fDataString, CP_UTF8, false);
+    {$endif HASCODEPAGE}
+    Text:= fDataString;
+  end
   else
   begin
     if Len - StartPos > L then

src/mormot.commit.inc
@@ -1,1 +1,1 @@
-'2.1.5077'
+'2.1.5078'

commit 8d3dc2b2679680d0901619e1d3477cdaac6de37f

properly fixed regression in TRawByteStringStream.GetAsText - introduced by https://github.com/synopse/mORMot2/commit/2c2a774bd - see https://synopse.info/forum/viewtopic.php?pid=39356#p39356

src/core/mormot.core.base.pas
@@ -11551,6 +11551,7 @@ begin
     SetCodePage(fDataString, CP_UTF8, false);
     {$endif HASCODEPAGE}
     Text := fDataString;
+    fDataString := ''; // release it ASAP to avoid multi-threading reuse bug
   end
   else
   begin

src/core/mormot.core.interfaces.pas
@@ -6314,7 +6314,7 @@ begin
         log.Timestamp64 := GetTickCount64;
         log.WasError := not result;
         log.Method := @aMethod;
-        FastSetString(log.Params, pointer(aParams), length(aParams));
+        log.Params := aParams;
         fLog.Add(log);
       end;
     end;

src/mormot.commit.inc
@@ -1,1 +1,1 @@
-'2.1.5085'
+'2.1.5086'

test/test.orm.threads.pas
@@ -335,8 +335,8 @@ begin
                 break
               else
               begin
-                fTest.Check(Rec.YearOfBirth = 1000 + i, 'yob');
-                fTest.Check(Rec.YearOfDeath = (1040 + i) and $ffff, 'yod');
+                fTest.CheckEqual(Rec.YearOfBirth, 1000 + i, 'yob');
+                fTest.CheckEqual(Rec.YearOfDeath, (1040 + i) and $ffff, 'yod');
                 //if (Rec.YearOfBirth<>1000+i) or (Rec.YearOfDeath<>1040+i) then writeln(i,'  ',ObjectToJSON(Rec));
                 if r = high(Rest) then
                   r := 0