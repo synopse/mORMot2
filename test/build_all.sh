c2c7fb6148945523ba16cf53a54f1edfd21f8d56
Arnaud Bouchez <ab@synopse.info>
2023-07-12 17:14:58 +0200

    new build_all script for tests on FPC - requires the cross-compilers to be available

---
+++ test/build_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ARCH=i386 TARGET=linux ./build_fpc.sh
+ARCH=x86_64 TARGET=linux ./build_fpc.sh
+ARCH=arm TARGET=linux ./build_fpc.sh
+ARCH=aarch64 TARGET=linux ./build_fpc.sh
+
+ARCH=i386 TARGET=win32 ./build_fpc.sh
+ARCH=x86_64 TARGET=win64 ./build_fpc.sh
+
+ARCH=x86_64 TARGET=darwin ./build_fpc.sh
+ARCH=aarch64 TARGET=darwin ./build_fpc.sh
