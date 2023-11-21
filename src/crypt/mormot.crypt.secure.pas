17a6746a4f806ddd408406e3a13a466309cb8e81
Arnaud Bouchez <ab@synopse.info>
2023-11-21 15:14:54 +0100

    hardened ASN.1 length decoding

--- src/crypt/mormot.crypt.secure.pas
+++ src/crypt/mormot.crypt.secure.pas
@@ -9344,11 +9344,14 @@ begin
     exit;
   n := result and $7f; // first byte is number of following bytes + $80
   result := 0;
-  repeat
-    result := (result shl 8) + cardinal(Buffer[Start]);
-    inc(Start);
-    dec(n);
-  until n = 0;
+  if n <= 3 then // not clearly out-of-range
+    repeat
+      result := (result shl 8) + cardinal(Buffer[Start]);
+      if integer(result) < 0 then
+        exit;
+      inc(Start);
+      dec(n);
+    until n = 0;
 end;
 
 function AsnEncInt(Value: Int64): TAsnObject;
