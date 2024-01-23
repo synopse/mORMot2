99db17e38483a246baf6ea61df82d45025efd65a
Arnaud Bouchez <ab@synopse.info>
2024-01-23 15:21:25 +0100

    rewritten FileCreate/FileOpen on Windows - FileCreate() includes a mode optional parameter - so that our MAX_PATH unlimited overloads would be used instead of the RTL versions

--- src/core/mormot.core.os.pas
+++ src/core/mormot.core.os.pas
@@ -2406,7 +2406,7 @@ function CloseHandle(hObject: THandle): BOOL; stdcall;
 /// redefined here to avoid warning to include "Windows" in uses clause
 // - why did Delphi define this slow RTL function as inlined in SysUtils.pas?
 // - also supports aFileName longer than MAX_PATH
-function FileCreate(const aFileName: TFileName): THandle;
+function FileCreate(const aFileName: TFileName; aMode: integer = 0): THandle; 
 
 /// redefined here to call CreateFileW() on non-Unicode RTL and support
 // aFileName longer than MAX_PATH
