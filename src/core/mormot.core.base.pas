2a20f88aa6c1a6b14a9c6cc9673d94724e66f330
Arnaud Bouchez <ab@synopse.info>
2024-02-15 15:44:28 +0100

    hardened and documented TSynLogInfo

--- src/core/mormot.core.base.pas
+++ src/core/mormot.core.base.pas
@@ -4020,6 +4020,7 @@ type
   /// the available logging events, as handled by mormot.core.log
   // - defined in mormot.core.base so that it may be used by the core units,
   // even if mormot.core.log is not explicitely linked
+  // - limited to 32 items, to efficiently fit in a 32-bit set
   // - sllInfo will log general information events
   // - sllDebug will log detailed debugging information
   // - sllTrace will log low-level step by step debugging information
@@ -12106,6 +12107,7 @@ var
   i, n: integer;
   crc: cardinal;
 begin
+  assert(ord(high(TSynLogInfo)) = 31);
   // initialize internal constants
   for i := 0 to 255 do
   begin
