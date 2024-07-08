de8c57715f4a2b019a7c8c0b65dcea45d59b5ecb
Arnaud Bouchez <ab@synopse.info>
2024-07-08 10:57:51 +0200

    defined TSynJsonFileSettings.SaveIfNeeded as function

--- src/core/mormot.core.json.pas
+++ src/core/mormot.core.json.pas
@@ -2589,7 +2589,8 @@ type
     function FolderName: TFileName;
     /// persist the settings as a JSON file, named from LoadFromFile() parameter
     // - will use the INI format if it was used at loading, or fsoWriteIni is set
-    procedure SaveIfNeeded; virtual;
+    // - return TRUE if file has been modified, FALSE if was not needed or failed
+    function SaveIfNeeded: boolean; virtual;
     /// optional persistence file name, as set by LoadFromFile()
     property FileName: TFileName
       read fFileName write fFileName;
@@ -11875,10 +11876,11 @@ begin
     result := ExtractFilePath(fFileName);
 end;
 
-procedure TSynJsonFileSettings.SaveIfNeeded;
+function TSynJsonFileSettings.SaveIfNeeded: boolean;
 var
   saved: RawUtf8;
 begin
+  result := false;
   if (self = nil) or
      (fFileName = '') or
      (fsoDisableSaveIfNeeded in fSettingsOptions) then
@@ -11891,6 +11893,7 @@ begin
     exit;
   FileFromString(saved, fFileName);
   fInitialJsonContent := saved;
+  result := true;
 end;
 
 
@@ -11921,6 +11924,7 @@ begin
   JSON_ESCAPE[13]  := ord('r');
   JSON_ESCAPE[ord('\')] := ord('\');
   JSON_ESCAPE[ord('"')] := ord('"');
+  // branchless JSON unescaping - default JSON_UNESCAPE_UNEXPECTED = #0
   for c := #32 to #127 do
     JSON_UNESCAPE[c] := c;
   JSON_UNESCAPE['b'] := #8;
@@ -11928,7 +11932,8 @@ begin
   JSON_UNESCAPE['n'] := #10;
   JSON_UNESCAPE['f'] := #12;
   JSON_UNESCAPE['r'] := #13;
-  JSON_UNESCAPE['u'] := JSON_UNESCAPE_UTF16;
+  JSON_UNESCAPE['u'] := JSON_UNESCAPE_UTF16; // = #1
+  // fast JSON parsing using JSON_CHARS[] and JSON_TOKENS[] lookup tables
   for c := low(c) to high(c) do
   begin
     if c in [#0, ',', ']', '}', ':'] then
@@ -11951,7 +11956,7 @@ begin
     if c in ['_', '0'..'9', 'a'..'z', 'A'..'Z', '.', '[', ']'] then
       include(JSON_CHARS[c], jcJsonIdentifier);
     if c in ['_', 'a'..'z', 'A'..'Z', '$'] then
-      // exclude '0'..'9' as already in jcDigitFirstChar
+      // exclude '0'..'9' as already in jtFirstDigit
       JSON_TOKENS[c] := jtIdentifierFirstChar;
   end;
   JSON_TOKENS[#0 ]  := jtEndOfBuffer;
