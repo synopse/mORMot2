synopse/mORMot2

commit 7488e6659213035ac1cf40359d39cb6d97924755

allow URI parmeters in the WebSockets URI - backport of https://github.com/synopse/mORMot/pull/455

src/mormot.commit.inc
@@ -1,1 +1,1 @@
-'2.2.7806'
+'2.2.7807'

src/net/mormot.net.ws.core.pas
@@ -591,7 +591,8 @@ type
     /// finalize the list storage
     destructor Destroy; override;
     /// create a new protocol instance, from the internal list
-    function CloneByName(const aProtocolName, aClientUri: RawUtf8): TWebSocketProtocol;
+    function CloneByName(
+      const aProtocolName, aClientUri: RawUtf8): TWebSocketProtocol;
     /// create a new protocol instance, from the internal list
     function CloneByUri(const aClientUri: RawUtf8): TWebSocketProtocol;
     /// how many protocols are stored
@@ -2214,23 +2215,25 @@ end;
 
 { TWebSocketProtocolList }
 
-function TWebSocketProtocolList.CloneByName(const aProtocolName,
-  aClientUri: RawUtf8): TWebSocketProtocol;
+function TWebSocketProtocolList.CloneByName(
+  const aProtocolName, aClientUri: RawUtf8): TWebSocketProtocol;
 var
   i: PtrInt;
+  uri: RawUtf8;
 begin
   result := nil;
   if self = nil then
     exit;
+  uri := Split(aClientUri, '?'); // ignore parameters
   fSafe.ReadLock;
   try
     for i := 0 to length(fProtocols) - 1 do
       with fProtocols[i] do
         if ((fUri = '') or
-            PropNameEquals(fUri, aClientUri)) and
+            PropNameEquals(fUri, uri)) and
            SetSubprotocol(aProtocolName) then
         begin
-          result := fProtocols[i].Clone(aClientUri);
+          result := fProtocols[i].Clone(uri);
           result.fName := aProtocolName;
           exit;
         end;
@@ -2243,17 +2246,19 @@ function TWebSocketProtocolList.CloneByUri(
   const aClientUri: RawUtf8): TWebSocketProtocol;
 var
   i: PtrInt;
+  uri: RawUtf8;
 begin
   result := nil;
+  uri := Split(aClientUri, '?');
   if (self = nil) or
-     (aClientUri = '') then
+     (uri = '') then
     exit;
   fSafe.ReadLock;
   try
     for i := 0 to length(fProtocols) - 1 do
-      if PropNameEquals(fProtocols[i].fUri, aClientUri) then
+      if PropNameEquals(fProtocols[i].fUri, uri) then
       begin
-        result := fProtocols[i].Clone(aClientUri);
+        result := fProtocols[i].Clone(uri);
         exit;
       end;
   finally

commit 9d1a54f69c68d525f53543cc580cd5a640a877a1

fixed regression in https://github.com/synopse/mORMot2/commit/7488e6659213035ac1cf40359d39cb6d97924755 - "with" is really dangerous ;)

src/mormot.commit.inc
@@ -1,1 +1,1 @@
-'2.2.7808'
+'2.2.7809'

src/net/mormot.net.http.pas
@@ -760,6 +760,7 @@ type
     fConnectionID: THttpServerConnectionID;
     fConnectionFlags: THttpServerRequestFlags;
     fAuthenticationStatus: THttpServerRequestAuthentication;
+    fInternalFlags: set of (ifUrlParamPosSet);
     fRespStatus: integer;
     fConnectionThread: TThread;
     fConnectionOpaque: PHttpServerConnectionOpaque;
@@ -771,6 +772,8 @@ type
     function GetRouteValuePosLen(const Name: RawUtf8;
       var Value: TValuePUtf8Char): boolean;
     function GetRouteValue(const Name: RawUtf8): RawUtf8;
+      {$ifdef HASINLINE} inline; {$endif}
+    function EnsureUrlParamPosExists: PUtf8Char;
       {$ifdef HASINLINE} inline; {$endif}
   public
     /// prepare an incoming request from a parsed THttpRequestContext
@@ -4604,28 +4607,33 @@ begin
   result := true;
 end;
 
+function THttpServerRequestAbstract.EnsureUrlParamPosExists: PUtf8Char;
+begin
+  result := fUrlParamPos;
+  if (result <> nil) or // may have been set by TUriTreeNode.LookupParam
+     (ifUrlParamPosSet in fInternalFlags) then
+    exit;
+  include(fInternalFlags, ifUrlParamPosSet); // call PosChar() once
+  result := PosChar(pointer(Url), '?');
+  fUrlParamPos := result;
+end;
+
 function THttpServerRequestAbstract.UrlParam(const UpperName: RawUtf8;
   out Value: RawUtf8): boolean;
 begin
-  if fUrlParamPos = nil then // may have been set by TUriTreeNode.LookupParam
-    fUrlParamPos := PosChar(pointer(Url), '?');
-  result := UrlDecodeParam(fUrlParamPos, UpperName, Value);
+  result := UrlDecodeParam(EnsureUrlParamPosExists, UpperName, Value);
 end;
 
 function THttpServerRequestAbstract.UrlParam(const UpperName: RawUtf8;
   out Value: cardinal): boolean;
 begin
-  if fUrlParamPos = nil then
-    fUrlParamPos := PosChar(pointer(Url), '?');
-  result := UrlDecodeParam(fUrlParamPos, UpperName, Value);
+  result := UrlDecodeParam(EnsureUrlParamPosExists, UpperName, Value);
 end;
 
 function THttpServerRequestAbstract.UrlParam(const UpperName: RawUtf8;
   out Value: Int64): boolean;
 begin
-  if fUrlParamPos = nil then
-    fUrlParamPos := PosChar(pointer(Url), '?');
-  result := UrlDecodeParam(fUrlParamPos, UpperName, Value);
+  result := UrlDecodeParam(EnsureUrlParamPosExists, UpperName, Value);
 end;
 
 procedure THttpServerRequestAbstract.SetOutJson(const Json: RawUtf8);

src/net/mormot.net.server.pas
@@ -2523,6 +2523,7 @@ begin
   result := false;
   if Len < 0 then // Pos^ = '?par=val&par=val&...'
   begin
+    include(req.fInternalFlags, ifUrlParamPosSet);
     req.fUrlParamPos := Pos; // for faster req.UrlParam()
     exit;
   end;
@@ -2934,6 +2935,7 @@ begin
   fOutContentType := '';
   fOutCustomHeaders := '';
   fAuthenticationStatus := hraNone;
+  fInternalFlags := [];
   fAuthenticatedUser := '';
   fErrorMessage := '';
   fUrlParamPos := nil;

src/net/mormot.net.ws.core.pas
@@ -2219,24 +2219,27 @@ function TWebSocketProtocolList.CloneByName(
   const aProtocolName, aClientUri: RawUtf8): TWebSocketProtocol;
 var
   i: PtrInt;
-  uri: RawUtf8;
+  u: RawUtf8;
+  p: TWebSocketProtocol;
 begin
   result := nil;
   if self = nil then
     exit;
-  uri := Split(aClientUri, '?'); // ignore parameters
+  u := Split(aClientUri, '?'); // ignore parameters
   fSafe.ReadLock;
   try
     for i := 0 to length(fProtocols) - 1 do
-      with fProtocols[i] do
-        if ((fUri = '') or
-            PropNameEquals(fUri, uri)) and
-           SetSubprotocol(aProtocolName) then
-        begin
-          result := fProtocols[i].Clone(uri);
-          result.fName := aProtocolName;
-          exit;
-        end;
+    begin
+      p := fProtocols[i];
+      if ((p.fUri = '') or
+          PropNameEquals(p.fUri, u)) and
+         p.SetSubprotocol(aProtocolName) then
+      begin
+        result := p.Clone(u);
+        result.fName := aProtocolName;
+        exit;
+      end;
+    end;
   finally
     fSafe.ReadUnLock;
   end;
@@ -2246,19 +2249,19 @@ function TWebSocketProtocolList.CloneByUri(
   const aClientUri: RawUtf8): TWebSocketProtocol;
 var
   i: PtrInt;
-  uri: RawUtf8;
+  u: RawUtf8;
 begin
   result := nil;
-  uri := Split(aClientUri, '?');
+  u := Split(aClientUri, '?');
   if (self = nil) or
-     (uri = '') then
+     (u = '') then
     exit;
   fSafe.ReadLock;
   try
     for i := 0 to length(fProtocols) - 1 do
-      if PropNameEquals(fProtocols[i].fUri, uri) then
+      if PropNameEquals(fProtocols[i].fUri, u) then
       begin
-        result := fProtocols[i].Clone(uri);
+        result := fProtocols[i].Clone(u);
         exit;
       end;
   finally