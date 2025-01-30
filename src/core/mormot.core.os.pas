5a0777517e989cc6e7578246e0269872f049f10a
Arnaud Bouchez <ab@synopse.info>
2025-01-30 18:49:13 +0100

    core: minor fix after https://github.com/synopse/mORMot2/pull/341

--- src/core/mormot.core.os.pas
+++ src/core/mormot.core.os.pas
@@ -4007,10 +4007,10 @@ function Utf8ToWin32PWideChar(const u: RawUtf8; var d: TSynTempBuffer): PWideCha
 // - as used internally by Utf8ToConsole()
 function Win32Utf8ToAnsi(P: pointer; L, CP: integer; var A: RawByteString): boolean;
 
-{$IfNDef NOEXCEPTIONINTERCEPT}
+{$ifndef NOEXCEPTIONINTERCEPT}
 /// get DotNet exception class names from HRESULT - published for testing purpose
 procedure Win32DotNetExceptions(code: cardinal; var names: TPShortStringDynArray);
-{$EndIf}
+{$endif NOEXCEPTIONINTERCEPT}
 
 /// ask the Operating System to convert a file URL to a local file path
 // - only Windows has a such a PathCreateFromUrlW() API
