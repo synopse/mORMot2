15e15f28f4d84d8cc1b88c99b42777de275e00cd
Arnaud Bouchez <ab@synopse.info>
2025-03-06 21:23:02 +0100

    core: new Keep0WithFolders optional parameter to PosixFileNames() - to include the folders, ending with a #0 char

--- src/core/mormot.core.os.pas
+++ src/core/mormot.core.os.pas
@@ -4006,9 +4006,11 @@ type
 // search, and may return some false positives, like symlinks or nested folders
 // - an optional callback can be supplied, used e.g. by the FileNames() function
 // in mormot.core.search to efficiently implement name mask search with a TMatch
+// - IncludeFoldersWith0 would include folder names, but with an ending #0 char
 function PosixFileNames(const Folder: TFileName; Recursive: boolean;
   OnFile: TOnPosixFileName = nil; OnFileOpaque: pointer = nil;
-  ExcludesDir: boolean = false; IncludeHiddenFiles: boolean = false): TRawUtf8DynArray;
+  ExcludesDir: boolean = false; IncludeHiddenFiles: boolean = false;
+  IncludeFoldersWith0: boolean = false): TRawUtf8DynArray;
 
 {$endif OSWINDOWS}
 
