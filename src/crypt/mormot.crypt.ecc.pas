98523893ef87382ea2e15df218f2249c5fa94072
Arnaud Bouchez <ab@synopse.info>
2025-07-27 19:17:50 +0200

    crypt: simplified TEccCertificateChain valid table - using lowest 128-bit of SHA-256 is safe and valid

--- src/crypt/mormot.crypt.ecc.pas
+++ src/crypt/mormot.crypt.ecc.pas
@@ -862,7 +862,6 @@ type
     fMaxVersion: byte;
     fIsValidCached: boolean;
     fIsValidCacheCount: integer;
-    fIsValidCacheSalt: RawByteString; // avoid flooding on forged input
     fIsValidCache: THash128DynArray;  // low TEccCertificateContent.ComputeHash
     function GetCount: integer;
       {$ifdef HASINLINE} inline; {$endif}
@@ -3708,7 +3707,6 @@ end;
 constructor TEccCertificateChain.Create;
 begin
   CreateVersion(2);
-  fIsValidCacheSalt := ToUtf8(RandomGuid); // avoid flooding on forged input
 end;
 
 constructor TEccCertificateChain.CreateFromJson(
@@ -3749,7 +3747,6 @@ function TEccCertificateChain.IsValidRaw(const content: TEccCertificateContent;
 var
   authoritypublickey: TEccPublicKey;
   hash: THash256Rec;
-  cached: THash128;
 begin
   // check certificate coherency and date before checking the cache
   result := ecvCorrupted;
@@ -3771,11 +3768,9 @@ begin
   if fIsValidCached then
   begin
     // try to recognize a previous valid certificate in the hash cache
-    cached := hash.Lo; // apply fIsValidCacheSalt and maybe AesNiHash128
-    DefaultHasher128(@cached, pointer(fIsValidCacheSalt), length(fIsValidCacheSalt));
     fSafe.ReadLock;
     try
-      if Hash128Index(pointer(fIsValidCache), fIsValidCacheCount, @cached) >= 0 then
+      if Hash128Index(pointer(fIsValidCache), fIsValidCacheCount, @hash) >= 0 then
         exit; // 128-bit lower part of sha-256 is very unlikely to collide
     finally
       fSafe.ReadUnlock;
@@ -3803,7 +3798,7 @@ begin
       try
         if fIsValidCacheCount > 1024 then
           fIsValidCacheCount := 0; // time to flush the cache once reached 16KB
-        AddHash128(fIsValidCache, cached, fIsValidCacheCount);
+        AddHash128(fIsValidCache, hash.Lo, fIsValidCacheCount);
       finally
         fSafe.WriteUnlock;
       end;
