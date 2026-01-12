ae7ece63dd3c407fce00f4f296137704cc6f1cd1
Arnaud Bouchez <ab@synopse.info>
2026-01-12 09:35:29 +0100

    core: compile libdeflate with PIC - is mandatory on some POSIX targets (e.g. MacOS linker) to generate libraries - PIC has no effect on Windows anyway

--- res/static/libdeflate/compile-fpc-x86_64-win64.sh
+++ res/static/libdeflate/compile-fpc-x86_64-win64.sh
@@ -11,7 +11,7 @@ echo Compiling for FPC on $FPCARCH using $GCC
 rm $DEST/libdeflatepas.a
 rm *.o
 
-$GCC -static -fno-pic -fno-stack-protector -O2 -m64 -fomit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables  -fno-unwind-tables -std=c99 -I. -Wall -Wundef -nostdlib -fvisibility=hidden -DWIN64 -D_ANSI_SOURCE  -c  lib/deflate_decompress.c lib/utils.c lib/x86/cpu_features.c lib/deflate_compress.c lib/adler32.c lib/crc32.c lib/zlib_decompress.c  lib/zlib_compress.c
+$GCC -static -fno-stack-protector -O2 -m64 -fomit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables  -fno-unwind-tables -std=c99 -I. -Wall -Wundef -nostdlib -fvisibility=hidden -DWIN64 -D_ANSI_SOURCE  -c  lib/deflate_decompress.c lib/utils.c lib/x86/cpu_features.c lib/deflate_compress.c lib/adler32.c lib/crc32.c lib/zlib_decompress.c  lib/zlib_compress.c
 
 strip -d -x *.o
 
