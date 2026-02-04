42c66917e5a3ca5b37e343acfb50bedf2af6f610
Martin Doyle <martin-doyle@online.de>
2026-02-04 21:34:38 +0100

    Updated - Project 03

--- ex/ThirdPartyDemos/martin-doyle/03-MethodBasedServices/src/main.pas
+++ ex/ThirdPartyDemos/martin-doyle/03-MethodBasedServices/src/main.pas
@@ -13,24 +13,37 @@ uses
   mormot.core.data,
   mormot.core.json,
   mormot.core.os,
+  mormot.core.text,
   mormot.core.unicode,
+  mormot.core.variants,
   mormot.orm.core,
+  mormot.rest.core,
   mormot.rest.http.client,
   data;
 
 type
   TMainForm = class(TForm)
-    ButtonAdd: TButton;
+    ButtonDelete: TButton;
     ButtonFind: TButton;
+    ButtonNew: TButton;
     ButtonQuit: TButton;
-    LabelEdit: TLabel;
-    NameEdit: TEdit;
-    QuestionMemo: TMemo;
-    procedure ButtonAddClick(Sender: TObject);
+    ButtonSave: TButton;
+    LabelName: TLabel;
+    LabelNames: TLabel;
+    EditName: TEdit;
+    ListNames: TListBox;
+    MemoQuestion: TMemo;
+    procedure ButtonDeleteClick(Sender: TObject);
     procedure ButtonFindClick(Sender: TObject);
+    procedure ButtonNewClick(Sender: TObject);
     procedure ButtonQuitClick(Sender: TObject);
+    procedure ButtonSaveClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure ListNamesClick(Sender: TObject);
+  private
+    function GetSelectedID: TID;
+    procedure RefreshNamesList;
   public
     HttpClient: TRestHttpClient;
     Model: TOrmModel;
@@ -46,35 +59,93 @@ implementation
 {
 ********************************** TMainForm ***********************************
 }
-procedure TMainForm.ButtonAddClick(Sender: TObject);
+function TMainForm.GetSelectedID: TID;
+begin
+  if ListNames.ItemIndex >= 0 then
+    Result := TID(ListNames.Items.Objects[ListNames.ItemIndex])
+  else
+    Result := 0;
+end;
+
+procedure TMainForm.RefreshNamesList;
+var
+  Response: RawUTF8;
+  Doc: TDocVariantData;
+  i: Integer;
+  ID: TID;
+  Name: RawUTF8;
+begin
+  ListNames.Items.Clear;
+  if HttpClient.CallBackGet('examplelist', [], Response) = HTTP_SUCCESS then
+  begin
+    Doc.InitJSON(Response);
+    for i := 0 to Doc.Count - 1 do
+    begin
+      ID := TID(Int64(Doc.Values[i].ID));
+      Name := VariantToUTF8(Doc.Values[i].Name);
+      ListNames.Items.AddObject(UTF8ToString(Name), TObject(ID));
+    end;
+  end;
+end;
+
+procedure TMainForm.ButtonNewClick(Sender: TObject);
+begin
+  EditName.Text := '';
+  MemoQuestion.Text := '';
+  ListNames.ItemIndex := -1;
+  EditName.SetFocus;
+end;
+
+procedure TMainForm.ButtonSaveClick(Sender: TObject);
 var
   Response: RawUTF8;
   Sample: TSample;
 begin
-  Sample.Name := StringToUTF8(NameEdit.Text);
-  Sample.Question := StringToUTF8(QuestionMemo.Text);
-
+  Sample.Name := StringToUTF8(EditName.Text);
+  Sample.Question := StringToUTF8(MemoQuestion.Text);
   if HttpClient.CallBackPut('example', RecordSave(Sample, TypeInfo(TSample)), Response) = HTTP_CREATED then
+    RefreshNamesList
+  else
+    ShowMessage('Error saving the data');
+end;
+
+procedure TMainForm.ButtonDeleteClick(Sender: TObject);
+var
+  Response: RawUTF8;
+  RecID: TID;
+begin
+  if ListNames.ItemIndex < 0 then
   begin
-    NameEdit.Text := '';
-    QuestionMemo.Text := '';
-    NameEdit.SetFocus;
+    ShowMessage('No record selected');
+    Exit;
+  end;
+  if MessageDlg('Delete this record?', mtConfirmation, [mbYes, mbNo], 0) <> mrYes then
+    Exit;
+  RecID := GetSelectedID;
+  if HttpClient.CallBack(mDELETE, 'example?ID=' + Int64ToUtf8(RecID), '', Response) = HTTP_SUCCESS then
+  begin
+    EditName.Text := '';
+    MemoQuestion.Text := '';
+    RefreshNamesList;
   end
   else
-    ShowMessage('Error adding the data');
+    ShowMessage('Error deleting the data');
 end;
 
 procedure TMainForm.ButtonFindClick(Sender: TObject);
 var
-  Response, Question: RawUTF8;
+  i: Integer;
+  SearchName: string;
 begin
-  if HttpClient.CallBackGet('example', ['Name', StringToUTF8(NameEdit.Text)], Response) = HTTP_SUCCESS then
-  begin
-    Question := JSONDecode(Response, 'Result', nil, false);
-    QuestionMemo.Text := UTF8ToString(Question);
-  end
-  else
-    QuestionMemo.Text := 'Not found';
+  SearchName := EditName.Text;
+  for i := 0 to ListNames.Items.Count - 1 do
+    if SameText(ListNames.Items[i], SearchName) then
+    begin
+      ListNames.ItemIndex := i;
+      ListNamesClick(nil);
+      Exit;
+    end;
+  ShowMessage('Not found');
 end;
 
 procedure TMainForm.ButtonQuitClick(Sender: TObject);
@@ -86,6 +157,7 @@ procedure TMainForm.FormCreate(Sender: TObject);
 begin
   Model := CreateSampleModel;
   HttpClient := TRestHttpClient.Create('localhost', HttpPort, Model);
+  RefreshNamesList;
 end;
 
 procedure TMainForm.FormDestroy(Sender: TObject);
@@ -94,14 +166,23 @@ begin
   Model.Free;
 end;
 
+procedure TMainForm.ListNamesClick(Sender: TObject);
+var
+  Response, Question: RawUTF8;
+  RecID: TID;
+  Name: RawUTF8;
+begin
+  RecID := GetSelectedID;
+  if RecID = 0 then
+    Exit;
+  // Find the name for this ID from the list
+  Name := StringToUTF8(ListNames.Items[ListNames.ItemIndex]);
+  if HttpClient.CallBackGet('example', ['Name', Name], Response) = HTTP_SUCCESS then
+  begin
+    Question := JSONDecode(Response, 'Result', nil, false);
+    EditName.Text := UTF8ToString(Name);
+    MemoQuestion.Text := UTF8ToString(Question);
+  end;
+end;
+
 end.
-
-
-
-
-
-
-
-
-
-
