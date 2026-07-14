ddf2e5bf3426bf811380163c3ed157c3bb6a6b16
Arnaud Bouchez <ab@synopse.info>
2026-07-14 14:17:52 +0200

    core: simplified TJsonWriter.AddJsonReformat - no functional change

--- src/core/mormot.core.json.pas
+++ src/core/mormot.core.json.pas
@@ -7025,16 +7025,15 @@ begin
   else
     start := nil;
   result := parser.Reformat(Json);
-  if start <> nil then // manual ending } of HJson implicit object
-  begin
-    if (jrfTrailingComma in parser.Fmt) and
-       not (parser.State in [stObjectNameFirst, stValueFirst]) then
-      AddDirect(',');
-    dec(fHumanReadableLevel);
-    if jrfIndent in parser.Fmt then
-      AddCRAndIndent;
-    AddDirect('}');
-  end;
+  if start = nil then // no manual ending } of HJson implicit object
+    exit;
+  if (jrfTrailingComma in parser.Fmt) and
+     not (parser.State in [stObjectNameFirst, stValueFirst]) then
+    AddDirect(',');
+  dec(fHumanReadableLevel);
+  if jrfIndent in parser.Fmt then
+    AddCRAndIndent;
+  AddDirect('}');
 end;
 
 procedure TJsonWriter.AddJsonEscape(P: pointer; Len: PtrInt);
