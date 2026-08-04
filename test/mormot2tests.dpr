461f9777e8f453cbdbd5aecf2db4638447dd16e9
Arnaud <ab@abmx>
2026-08-05 00:12:15 +0200

    test: fixed mormot2tests compilation

--- test/mormot2tests.dpr
+++ test/mormot2tests.dpr
@@ -148,19 +148,13 @@ begin
   AddCase([
     TTestCoreBase,
     TTestCoreProcess,
-    TTestCoreXml,
-    TTestCoreYaml,
     {$ifdef HASGENERICS} // do-nothing on oldest compilers (e.g. <= Delphi XE7)
     TTestCoreCollections,
     {$endif HASGENERICS}
     TTestCoreCrypto,
     TTestCoreEcc,
     TTestCoreCompression,
-    TNetworkProtocols,
-    // TTestCoreYaml registered last: dvoInternNames interning would otherwise
-    // pollute the TDocVariant intern counters that the JSON benchmark of
-    // TTestCoreProcess measures as deltas
-    TTestCoreYaml
+    TNetworkProtocols
   ]);
 end;
 
