e1036ba54ce93636e028ba5517a439e820befccb
Arnaud Bouchez <ab@synopse.info>
2026-09-16 13:45:17 +0200

    core: new FORCE_SSSE3_JSON conditional

--- src/core/mormot.core.variants.pas
+++ src/core/mormot.core.variants.pas
@@ -3113,6 +3113,7 @@ function GetNumericVariantPas(Json: PUtf8Char;
 var
   /// redirect to GetNumericVariantPas by default which seems the fastest
   // - you may try GetNumericVariantSsse3() for SIMD process on longer numbers
+  // e.g. by setting FORCE_SSSE3_JSON conditional at compilation time
   GetNumericVariantStub: TGetNumericVariantFromJson = GetNumericVariantPas;
 {$endif ASMX64NOTPIC}
 
@@ -14019,6 +14020,12 @@ begin
   except // paranoid to avoid fatal exception during process initialization
   end;
   {$endif FPC}
+  {$ifdef FORCE_SSSE3_JSON}
+  {$ifdef ASMX64NOTPIC}
+  if cpuSSSE3 in X64CpuFeatures then
+    GetNumericVariantStub := @GetNumericVariantSsse3;
+  {$endif ASMX64NOTPIC}
+  {$endif FORCE_SSSE3_JSON}
 end;
 
 
