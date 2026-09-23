9de9bf9b4c58f084f79494fcac6789962b548f4c
Arnaud Bouchez <ab@synopse.info>
2026-09-23 18:09:58 +0200

    net: enhanced TSChannelNetTls - especially better Wine support

--- src/core/mormot.core.base.pas
+++ src/core/mormot.core.base.pas
@@ -3679,7 +3679,7 @@ procedure MultiEventMerge(var DestList; const ToBeAddedList);
 
 /// compare two TMethod instances
 function EventEquals(const eventA, eventB): boolean;
-  {$ifdef HASSAFEINLINE}inline;{$endif}
+  {$ifdef FPC}inline;{$endif}
 
 
 { ************ Buffers (e.g. Hashing and SynLZ compression) Raw Functions }
